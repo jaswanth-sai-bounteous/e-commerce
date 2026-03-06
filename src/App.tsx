e7ec74f543214b5de4337ed4d00f804d32f0df79
Panchumarthi Jaswanth Sai <panchumarthi.sai@bounteous.com>
2026-03-06 10:32:39 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { lazy, Suspense } from "react"
 import MainLayout from "./components/layout/MainLayout"
-import Home from "./pages/Home"
-import PLP from "./pages/PLP"
-import PDP from "./pages/PDP"
- import Cart from "./pages/Cart"
-import Checkout from "./pages/Checkout"
+
+
+const Home = lazy(() => import("./pages/Home"))
+const PLP = lazy(() => import("./pages/PLP"))
+const PDP = lazy(() => import("./pages/PDP"))
+const Cart = lazy(() => import("./pages/Cart"))
+const Checkout = lazy(() => import("./pages/Checkout"))
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<PLP />} />
-          <Route path="/products/:id" element={<PDP />} />
-         
-
-<Route path="/cart" element={<Cart />} />
-  <Route path="/checkout" element={<Checkout />} />
-
-        </Route>
-      </Routes>
+      <Suspense fallback={<div className="p-10 text-center">Loading...</div>}>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<PLP />} />
+            <Route path="/products/:id" element={<PDP />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,12 +3,20 @@ import type { Product } from "../types/product"
 
 
 
-const API_URL = "https://fakestoreapi.com"
+const API_URL = "https://ecom-backend-withpg-2.onrender.com"
 
 export const productService = {
   getAll: async (): Promise<Product[]> => {
     const res = await axios.get(`${API_URL}/products`)
-    return res.data
+
+    return res.data.products.map((p: any) => ({
+      id: p.id,
+      title: p.title,
+      price: p.price,
+      description: p.description,
+      category: p.category,
+      image: p.product_img   // map backend field → frontend field
+    }))
   },
 
   getById: async (id: string): Promise<Product> => {
